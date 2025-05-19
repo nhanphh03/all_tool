ecc57e0edbc36f3eeae0948a7015e767e55bc926
Pham Huu Nhan <102335308+nhanphh03@users.noreply.github.com>
2025-05-19 17:28:03 +0700

    Update background.js

--- autofill_tool/background.js
+++ autofill_tool/background.js
@@ -2,15 +2,14 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url.includes('logoform.jp')) {
-    chrome.storage.local.get('autoFillEnabled', (data) => {
-      if (data.autoFillEnabled) {
-        chrome.scripting.executeScript({
-          target: { tabId: tabId },
-          files: ['content.js']
+    if (changeInfo.status === 'complete' && tab.url.includes('logoform.jp')) {
+        chrome.storage.local.get('autoFillEnabled', (data) => {
+            if (data.autoFillEnabled) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId },
+                    files: ['contentv2.js']
+                });
+            }
         });
-      }
-    });
-  }
+    }
 });
-
