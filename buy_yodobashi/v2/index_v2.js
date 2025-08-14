e0f4867f59f33d98bdcceef6d8268462f06e5f54
nhanph <nhanphh2003@gmail.com>
2025-08-14 22:52:24 +0700

    tool auto buy yodobashi

--- buy_yodobashi/v2/index_v2.js
+++ buy_yodobashi/v2/index_v2.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const {chromium} = require('playwright');
-const config = require('../config');
+const config = require('./config');
 const {
     configBrowser,
     waitUntilTime,
@@ -11,7 +11,7 @@ const {
     proceedToCheckoutStep2,
     placeOrder,
     enterSecurityCode
-} = require("../function");
+} = require("./function");
 
 
 (async () => {
@@ -45,43 +45,52 @@ const {
         // Sử dụng hàm đã tách
         const pagesMain = await configBrowser(links, users, browser, jsonConfig);
 
-        console.log('⏳ Bắt đầu hẹn giờ chạy chương trình');
-        console.log("\n")
+        console.log('⏳ Bắt đầu hẹn giờ chạy chương trình \n');
 
         await (async () => {
             await waitUntilTime(jsonConfig.hourRun, jsonConfig.minuteRun, jsonConfig.secondRun);
-            console.log("Chạy code sau khi đủ giờ");
-            console.log("\n")
+            console.log("Chạy code sau khi đủ giờ \n");
         })();
 
         const startBuy = new Date();
 
+        console.log("⏳ Bắt đầu mua hàng lúc ", startBuy.toLocaleTimeString('vi-VN') , " \n");
+
+        const allPages = pagesMain.flatMap(browser =>
+            browser.page.map(p => p.page)
+        );
+        await reloadAllPages(allPages);
+
+        console.log(pagesMain)
+
         for (const browser of pagesMain) {
-            console.log("⏳ Bắt đầu mua hàng lúc ", startBuy.toLocaleTimeString('vi-VN'));
-            console.log("\n")
-            await reloadAllPages(browser.page);
+            console.log("-------------------------------------------------------------------------")
+            const startChild = new Date();
+            console.log("Bắt đầu mua hàng cho tài khoản --- ", browser.user.username)
             const cvv = browser.user.cvv;
-            console.log(cvv)
-            for (const page of browser.page) {
-                await addProductToCard( page );
-                await delay(1000);
-                await proceedToCheckoutStep1( page );
-                await delay(1000);
-                await proceedToCheckoutStep2( page );
-                await delay(1000);
-                await enterSecurityCode( page, cvv );
-                await delay(1000);
-                await placeOrder( page );
-                await delay(1000);
-                console.log("Đặt thành công đơn hàng !")
-                console.log("\n")
-                // await proceedWith3DSecure( page );
+            for (const { page, quantity } of browser.page) {
+                await addProductToCard( page, quantity );
             }
+            const page = null;
+            // await proceedToCheckoutStep1( page );
+            // await delay(1000);
+            // await proceedToCheckoutStep2( page );
+            // await delay(1000);
+            // await enterSecurityCode( page, cvv );
+            // await delay(1000);
+            // await placeOrder( page );
+            // await delay(1000);
+            console.log("Đặt thành công đơn hàng !")
+            // await proceedWith3DSecure( page );
+
+
+            const endChild = new Date();
+            console.log("Kết thúc mua hàng cho tài khoản --- ", browser.user.username, " tổng thời gian ", endChild - startChild);
+            console.log("--------------------------------------------------------------------------")
         }
 
-        console.log(`⏳ Tổng thời gian từ lúc bắt đầu mở tool: ${new Date().getTime() - start.getTime()}ms`);
-        console.log("\n")
-        console.log(`⏳ Tổng thời gian từ lúc bắt đầu mua hàng: ${new Date().getTime() - startBuy.getTime()}ms`);
+        console.log(`⏳ Tổng thời gian từ lúc bắt đầu mở tool: ${new Date().getTime() - start.getTime()}ms \n`);
+        console.log(`⏳ Tổng thời gian từ lúc bắt đầu mua hàng: ${new Date().getTime() - startBuy.getTime()}ms \n`);
 
     } catch (error) {
         console.error('Main Error:', error.message);
